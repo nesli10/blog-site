591b325868de34dbdbffbf98f6d9df4bc3bb5e84
neslişah ebral <nestay2001@gmail.com>
2022-10-25 20:01:58 +0300

    install ant desingn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
+import 'antd/dist/antd.css';
 function App() {
   return (
     <div >
+      
+     
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import 'antd/dist/antd.css';
 
+import App from './App';
+import { DatePicker } from 'antd';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-    <App />
-  
+ 
+    <App/>
+   
 );
 
